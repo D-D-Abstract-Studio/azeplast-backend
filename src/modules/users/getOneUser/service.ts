755a2892e869b7c46f686ee65e06b45bc4404e4c
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-24 22:47:17 -0300

    feat: Refactor getOneUserService to use findById instead of findOne

--- src/modules/users/getOneUser/service.ts
+++ src/modules/users/getOneUser/service.ts
@@ -3,11 +3,11 @@ import { HTTPError } from '@/errors'
 import { User } from '@/models/User'
 
 export const getOneUserService = async (id: string) => {
-  const userDocument = await User.findOne({ _id: id })
+  const user = await User.findById(id)
 
-  if (!userDocument) {
+  if (!user) {
     throw new HTTPError('User not found', 404)
   }
 
-  return userDocument
+  return user
 }
