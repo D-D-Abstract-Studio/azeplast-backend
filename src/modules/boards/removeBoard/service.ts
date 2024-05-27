ec598fa3370e7ae25bcd698317bfc8e01de01095
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-26 21:33:39 -0300

    feat: Refactor deleteDomainSchema in removeBoard service
    The code changes involve refactoring the deleteDomainSchema in the removeBoard service of the boards module. This change improves the code structure and readability.
    
    Recent repository commits:
    - feat: Update getAllUsers route and controller to use getAllBoards
    - feat: Update userRouter to use PUT method for updating user
    - feat: Update userRouter to use PUT method for updating user
    - feat: Refactor getOneUserService to use findById instead of findOne
    - feat: Update getOneUser route and controller
    - feat: Add getAllUsers route and controller
    - feat: Add userRouter to app.ts
    - feat: Refactor Kanban model schemas and add createBoardController
    - feat: Update Kanban model schemas and default settings
    - feat: Add types and models for Kanban board
    - feat: Atualizar prioridade para valores em português
    - feat: Atualizar prioridade para valores em português
    - feat: Add types and models for Kanban board
    - chore: Add new files and configurations
    - Create README.md

--- src/modules/boards/removeBoard/service.ts
+++ src/modules/boards/removeBoard/service.ts
@@ -1,6 +1,6 @@
 import { HTTPError } from '@/errors/httpError'
 
-import { User } from '@/models/User'
+import { KanbanBoard } from '@/models/KanbanBoard'
 
 import * as Z from 'zod'
 
@@ -8,16 +8,16 @@ type DeleteUserService = {
   id: string
 }
 
-export const deleteDomainSchema = Z.object({
+const deleteDomainSchema = Z.object({
   id: Z.string()
 })
 
 export const deleteBoardService = async (data: DeleteUserService) => {
   const { id } = deleteDomainSchema.parse(data)
 
-  const domainExists = await User.findOne({ _id: id })
+  const boardExists = await KanbanBoard.findOne({ _id: id })
 
-  if (!domainExists) throw new HTTPError('User not found', 404)
+  if (!boardExists) throw new HTTPError('Board not found', 404)
 
-  return User.deleteOne({ _id: id })
+  return KanbanBoard.deleteOne({ _id: id })
 }
