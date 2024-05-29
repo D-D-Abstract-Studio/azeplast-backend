13d0d78075ed9aee7d531e78ded2d825237e5aaf
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-05-29 15:11:13 -0300

    feat: Update updateTasks module to use updateTaskController

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,18 @@
+const process = {
+  env: {
+    PORT: '8000',
+    MONGODB_PORT: '27017',
+    MONGO_DOMAIN: '127.0.0.1',
+    MONGODB_USERNAME: 'div',
+    MONGODB_PASSWORD: '20020000',
+    MONGODB_DATABASE: 'azePlast',
+    JWT_SECRET: 'yqutyvxijqbiugc8976t23971g2ydw796fc5f237vucyva8yf725r3821ouqygd8632fx2v3y',
+    JWT_EXPIRATION_TIME: '10',
+    MAIL_PASSWORD: 'erfhsokepljtfszn',
+    MAIL_USERNAME: 'developers@azePlast.com.br'
+  }
+}
+
 const MILISECONDS_PER_SECOND = 1000
 const SECONDS_PER_MINUTE = 60
 const MINUTES_PER_HOUR = 60
