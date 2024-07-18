8705285c334a5c3906382a853dd981bc359a54af
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-07-18 08:23:09 -0300

    refactor: Remove unused KanbanHistory module
    The code changes remove the unused KanbanHistory module, including the model, controller, and service files. This module was no longer being used in the application and has been removed to improve code organization and reduce clutter.
    
    Recent repository commits:
    - feat: Update updateTasks module to use updateTaskController
    - feat: Add getOneTaskController to fetch a single task by ID
    - feat: Update createTaskController to use spread operator for req.body
    - feat: Update TaskSchema in validations module
    - feat: Update createTasksService to use TaskSchema for data validation
    - feat: Update deleteBoardController to use boardId parameter
    - feat: Add ordered field to KanbanBoard model
    - feat: Update getAllTasksController to fetch tasks instead of columns
    - feat: Add createColumnController and createColumnService
    - feat: Update deleteBoardController to include archived flag
    - feat: Add boardId field to KanbanColumn model
    - feat: Refactor deleteDomainSchema in removeBoard service
    - refactor: Rename removeUser to removeBoard in board module
    - feat: Update getAllUsers route and controller to use getAllBoards
    - feat: Update userRouter to use PUT method for updating user
    - feat: Refactor getOneUserService to use findById instead of findOne
    - feat: Update getOneUser route and controller
    - feat: Add getAllUsers route and controller
    - feat: Update userRouter to include userId parameter in patch route
    - feat: Update getOneUser route and controller
    - feat: Update createTaskController to use spread operator for req.body
    - feat: Update TaskSchema in validations module
    - feat: Update createTasksService to use TaskSchema for data validation
    - feat: Update deleteBoardController to use boardId parameter
    - feat: Add ordered field to KanbanBoard model
    - feat: Update getAllTasksController to fetch tasks instead of columns
    - feat: Add createColumnController and createColumnService
    - feat: Update deleteBoardController to include archived flag
    - feat: Add boardId field to KanbanColumn model
    - feat: Refactor deleteDomainSchema in removeBoard service
    - refactor: Rename removeUser to removeBoard in board module
    - feat: Update getAllUsers route and controller to use getAllBoards
    - feat: Update userRouter to use PUT method for updating user
    - feat: Refactor getOneUserService to use findById instead of findOne
    - feat: Update getOneUser route and controller
    - feat: Add getAllUsers route and controller
    - feat: Update userRouter to include userId parameter in patch route
    - feat: Update getOneUser route and controller

--- src/modules/histories/getAllHistories/controller.ts
+++
@@ -1,9 +0,0 @@
-import { getAllHistoriesService } from './service'
-
-import type { RequestHandler } from 'express'
-
-export const getAllHistoriesController: RequestHandler = async (req, res) => {
-  const histories = await getAllHistoriesService()
-
-  return res.status(201).json(histories)
-}
