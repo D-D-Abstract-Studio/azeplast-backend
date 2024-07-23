9bc723451a675b311e06466effd984c3c7c0041f
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-07-23 07:48:25 -0300

    refactor: Update dbUrl in dbMongo configuration

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
-const process = {
-  env: {
-    PORT: '8000',
-    MONGODB_PORT: '27017',
-    MONGO_DOMAIN: '127.0.0.1',
-    MONGODB_USERNAME: 'div',
-    MONGODB_PASSWORD: '20020000',
-    MONGODB_DATABASE: 'azePlast',
-    JWT_SECRET: 'yqutyvxijqbiugc8976t23971g2ydw796fc5f237vucyva8yf725r3821ouqygd8632fx2v3y',
-    JWT_EXPIRATION_TIME: '10',
-    MAIL_PASSWORD: 'erfhsokepljtfszn',
-    MAIL_USERNAME: 'developers@azePlast.com.br'
-  }
+export const HOST_API = process.env.NODE_ENV === 'production' ? 'http://192.168.2.15' : 'http://localhost:8000'
+
+const environment = {
+  PORT: '8000',
+  MONGODB_PORT: '27017',
+  MONGO_DOMAIN: '127.0.0.1',
+  MONGODB_USERNAME: 'div',
+  MONGODB_PASSWORD: '20020000',
+  MONGODB_DATABASE: 'azePlast',
+  JWT_SECRET: 'yqutyvxijqbiugc8976t23971g2ydw796fc5f237vucyva8yf725r3821ouqygd8632fx2v3y',
+  JWT_EXPIRATION_TIME: '10',
+  MAIL_PASSWORD: 'erfhsokepljtfszn',
+  MAIL_USERNAME: 'developers@azePlast.com.br'
 }
 
 const MILISECONDS_PER_SECOND = 1000
@@ -18,7 +18,7 @@ const SECONDS_PER_MINUTE = 60
 const MINUTES_PER_HOUR = 60
 const HOURS_PER_DAY = 24
 
-const DAYS = Number(process.env.JWT_EXPIRATION_TIME) || 1
+const DAYS = Number(environment.JWT_EXPIRATION_TIME) || 1
 
 const milisecondsPerDay = MILISECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR * HOURS_PER_DAY
 
@@ -26,17 +26,17 @@ const jwtExpirationTime = DAYS * milisecondsPerDay
 
 export const env = {
   jwtExpirationTime,
-  jwtSecret: process.env.JWT_SECRET,
-  dbPort: process.env.MONGODB_PORT,
-  dbDomain: process.env.MONGO_DOMAIN,
-  dbDatabase: process.env.MONGODB_DATABASE,
-  dbUser: process.env.MONGODB_USERNAME,
-  dbPassword: process.env.MONGODB_PASSWORD
+  jwtSecret: environment.JWT_SECRET,
+  dbPort: environment.MONGODB_PORT,
+  dbDomain: environment.MONGO_DOMAIN,
+  dbDatabase: environment.MONGODB_DATABASE,
+  dbUser: environment.MONGODB_USERNAME,
+  dbPassword: environment.MONGODB_PASSWORD
 }
 
 export const dbMongo = {
   ...env,
-  dbUrl: `mongodb://${process.env.MONGO_DOMAIN}:${process.env.MONGODB_PORT}/${env.dbDatabase}?authSource=admin`
+  dbUrl: `mongodb://${environment.MONGO_DOMAIN}:${environment.MONGODB_PORT}/${env.dbDatabase}?authSource=admin`
 }
 
 export const collectionsData = {
--- src/shared/translate-text/index.ts
+++ src/shared/translate-text/index.ts
@@ -3,10 +3,6 @@ import translatte from 'translatte'
 import type { Translate } from './type'
 
 export const translateText = async (message: string) => {
-  {
-    process.env.NODE_ENV === 'development' && console.log(message)
-  }
-
   try {
     const text = await translatte<Translate>(message, {
       to: 'pt',
