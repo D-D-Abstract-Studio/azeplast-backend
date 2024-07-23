c788807dc806d9ebddd68e018de092b229549f42
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-07-23 09:22:34 -0300

    feat: Update createUploadsService to use file.filename instead of originalNameUtf8
    This commit updates the `createUploadsService` function in the `createUploads` module to use `file.filename` instead of `originalNameUtf8` for the `name` property of the returned object. This change ensures that the correct filename is used for the uploaded file.

--- src/modules/uploads/createUploads/service.ts
+++ src/modules/uploads/createUploads/service.ts
@@ -2,10 +2,9 @@ import { HOST_API } from '@/config'
 
 export const createUploadsService = async ({ files }: { files: Express.Multer.File[] }) => {
   const newfiles = files.map(file => {
-    const originalNameUtf8 = Buffer.from(file.originalname, 'latin1').toString('utf8').replace(/_/g, '_')
-
     return {
-      name: originalNameUtf8,
+      ...file,
+      name: file.filename,
       size: file.size,
       type: file.mimetype,
       preview: `${HOST_API}/${file.path.split('src/')[1]}`
